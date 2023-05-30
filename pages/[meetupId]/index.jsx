bb38f325e09b14de15d518a772d97a7ef586380e
Keshav Sharma <keshav2743@gmail.com>
2023-05-30 23:30:20 +0530

    handling page errors

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,39 +1,126 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
 import Head from "next/head";
+import { connectMongo } from "@/libs/connectMongo";
+import Meetup from "@/models/MeetUp";
+import mongoose from "mongoose";
 
 const MeetupDetails = (props) =>{
+
+    if(props.error)
+    {
+        return( <>
+        <Head >
+            <title>Item not found</title>
+            <meta name="description" content="Not found" />
+        </Head>
+         <h1>Not found</h1>
+        </>
+        )
+    }
+
+ 
+
+    
     
     return (
         <>
         <Head >
-            <title>{props.meetupData.title}</title>
-            <meta name="description" content={props.meetupData.description} />
+            <title>{props.title}</title>
+            <meta name="description" content={props.description} />
         </Head>
         <MeetupDetail 
-        image={props.meetupData.image} 
-        title={props.meetupData.title} 
-        address={props.meetupData.address} 
-        description={props.meetupData.description} 
+        image={props.image} 
+        title={props.title} 
+        address={props.address} 
+        description={props.description} 
         />
         </>
     )
+
 }
-export async function getStaticPaths(){
-   
-    const response = await(await fetch(process.env.BASE_URL+'/api/get-meetup')).json();
-    const data = response.data;
-   
-   
+
+export async function getStaticPaths()
+{
+    connectMongo();
+    const data = await Meetup.find({});
+    
     const path = data.map((d) => ({
         params: { meetupId: d._id.toString() },
       }))
+      return {
+        fallback: true,
+        paths: path,
+       
+    }
+
+}
+
+
+export async function getStaticProps(context)
+{
+    connectMongo();
+    const id = context.params.meetupId;
+   
+    if(!mongoose.Types.ObjectId.isValid(id))
+    {
+        return{
+            props: {
+                error:true
+            }
+        }
+    }
+    
+    
+    const data = await Meetup.findById(id);
+    if(!data.title)
+    {
+        return{
+            props: {
+                error:true
+            }
+        }
+    }
+   
+    
+    return {
+        props: {
+                image: data.image,
+                id: data._id.toString(),
+                title: data.title,
+                address: data.address,
+                description: data.description
+            
+        }
+    }
+
+
+
+
+
+}
+
+// export async function getStaticPaths(){
+   
+//     const response = await(await fetch(process.env.BASE_URL+'/api/get-meetup',
+//     {
+//         headers:{
+//             'Content-Type': 'application/json'
+//         }
+//     }
+//     )).json();
+//     const data = response.data;
+   
+   
+//     const path = data.map((d) => ({
+//         params: { meetupId: d._id.toString() },
+//       }))
 
    
-    return {
-        paths: path,
-        fallback: true,
-    }
-}
+//     return {
+//         paths: path,
+//         fallback: true,
+//     }
+// }
 // export async function getServerProps(context){
 //     const meetId = context.params.meetupId;
 //     console.log(meetId);
@@ -49,26 +136,25 @@ export async function getStaticPaths(){
 //         }
 //     }
 // } 
-export async function getStaticProps(context){
-    const id = context.params.meetupId; 
+// export async function getStaticProps(context){
+//     const id = context.params.meetupId; 
     
-    const response = await(await fetch(process.env.BASE_URL+`/api/get-meetup/${id}`)).json();
-  const data = response.data;
+//     const response = await(await fetch(process.env.BASE_URL+`/api/get-meetup/${id}`)).json();
+//   const data = response.data;
    
     
-    return {
-        props: {
-            meetupData: {
-                image: data.image,
-                id: data._id,
-                title: data.title,
-                address: data.address,
-                description: data.description
-            }
-        },
-        revalidate: 1000
-    }
+//     return {
+//         props: {
+//             meetupData: {
+//                 image: data.image,
+//                 id: data._id,
+//                 title: data.title,
+//                 address: data.address,
+//                 description: data.description
+//             }
+//         }
+//     }
 
-}
+// }
 
 export default MeetupDetails;
